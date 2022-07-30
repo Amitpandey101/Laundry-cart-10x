10f06215ff7adb9c2938ded1ab42cc2e036ac0b4
BhaveshM11 <bhavesh.mandle@gmail.com>
2022-07-30 12:41:14 +0530

    Added server folder

---
+++ server/controller/signupauth.js
@@ -0,0 +1,22 @@
+const UserData = require('../models/registeredUser');
+
+
+const UniqueInputAuth = (req,res,next)=>{
+    console.log(req.body)
+    UserData.find({email:req.body.email})
+    .then((data)=>{
+        console.log(data)
+        if(data.length !== 0 ){
+           return res.status(401).json({error:'Email has Already registered'})
+        }else{
+            UserData.find({phone:req.body.phone}).then((data)=>{
+                if(data.length !== 0){
+                   return res.status(401).json({error:'phone no. already registered'})
+                }else{
+                    next();
+                }
+            })
+        }
+    })
+}
+module.exports = UniqueInputAuth;
---
+++ server/routes/routes.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router();
+
+const signUpAuth = require('../controller/signupauth');
+const loginAuth = require('../controller/loginauth')
+const userdata = require('../controller/userdata');
+const authorization = require('../controller/authorization');
+
+
+
+
+router.post('/register',signUpAuth, userdata)
+
+router.post('/login',loginAuth)
+
+router.get('/userdata',authorization)
+
+router.get('/',(req,res)=>{
+    res.send("Welcome to Base Page")
+})
+
+module.exports = router;
