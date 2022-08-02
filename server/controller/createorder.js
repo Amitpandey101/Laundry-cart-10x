3132ea6c8192a48845d77a445bf878b5b715ef3c
yogesh <jd11rock@gmail.com>
2022-08-02 12:10:21 +0530

    added summary

--- server/controller/createorder.js
+++ server/controller/createorder.js
@@ -3,20 +3,22 @@ const OrderData = require("../models/order")
 const UserData = require('../models/registeredUser');
 
 const createorder= async (req,res)=>{
+    console.log(req.body.data.data)
+    console.log(req.body.headers.authorization)
     try{
-        const {email} = jwt.verify(req.headers.authorization,process.env.SECRET_KEY)
+        const {email} = jwt.verify(req.body.headers.authorization,process.env.SECRET_KEY)
         UserData.find({email:email}).then((userdata)=>{
          if(userdata.length){
-             OrderData.create({date:req.body.date,totalPrice:req.body.totalPrice,totalItems:req.body.totalItems,status:req.body.status,products:req.body.products,user:userdata[0].email}).then((order)=>{
-          res.send({"status":"order submitted","data":{"body":order}})
+             OrderData.create({date:req.body.data.date,email:email,order:req.body.data.order,Subtotal:req.body.data.Subtotal,Total:req.body.data.Total}).then((order)=>{
+                res.json({message:'order submitted successfully!!!'})
              })
          }else{
-             res.status(400).send({"status":"unauthorized User"})
+             res.json({error:"unauthorized User"})
          }
         }) 
     }
     catch(err){
-        res.status(400).send({"status":"unauthorized User"})
+        res.json({error:"failed!"})
     }
   
 }
